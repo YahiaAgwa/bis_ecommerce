7ec5b9b9af736d5245b0c55205be399074b9af75
Yahia Agwa <yahia.agwa@gmail.com>
2021-09-04 13:35:41 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+import 'pages/home.dart';
+
+void main() {
+  runApp(MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
---
+++ lib/pages/home.dart
@@ -0,0 +1,168 @@
+import 'dart:developer';
+
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  _cartPressed() {
+    log('you clicked the cart icon');
+  }
+
+  List<dynamic> _listItems = [
+    {'title': 'Home', 'icon': Icons.home},
+    {'title': 'Categories', 'icon': Icons.category},
+    {'title': 'Offers', 'icon': Icons.money},
+    {'title': 'Brands', 'icon': Icons.branding_watermark},
+    {'title': 'Contact Us', 'icon': Icons.call},
+    {'title': 'About Us', 'icon': Icons.info},
+    {'title': 'Settings', 'icon': Icons.settings}
+  ];
+  TextEditingController _searchController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        bottom: PreferredSize(
+          preferredSize: Size.fromHeight(40),
+          child: Container(
+            height: 40,
+            decoration: BoxDecoration(color: Colors.white, borderRadius: BorderRadius.circular(20)),
+            margin: EdgeInsets.all(10),
+            padding: EdgeInsets.all(5),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: _searchController,
+                    decoration: InputDecoration.collapsed(hintText: 'Search'),
+                  ),
+                ),
+                IconButton(
+                    onPressed: () {
+                      log(_searchController.text);
+                    },
+                    icon: Icon(Icons.search))
+              ],
+            ),
+          ),
+        ),
+        title: Image.asset(
+          'assets/logo.png',
+          width: 200,
+        ),
+        actions: [
+          IconButton(
+            onPressed: _cartPressed,
+            icon: Icon(Icons.shopping_basket),
+          ),
+        ],
+      ),
+      drawer: Drawer(
+        child: Column(
+          children: [
+            Container(
+              height: 250,
+              color: Colors.blue,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  SizedBox(
+                    height: 30,
+                  ),
+                  ListTile(
+                    leading: CircleAvatar(
+                      radius: 40,
+                    ),
+                    title: Text('Hell Guest'),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        ElevatedButton(onPressed: () {}, child: Text('Login')),
+                        Switch(
+                          value: true,
+                          onChanged: (val) {},
+                          activeColor: Colors.green,
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: _listItems.length,
+                itemBuilder: (context, i) => ListTile(
+                  leading: Icon(_listItems[i]['icon']),
+                  title: Text(_listItems[i]['title']),
+                ),
+              ),
+            ),
+            ListTile(
+              title: Text('Terms'),
+              leading: Icon(Icons.shield),
+            )
+          ],
+        ),
+      ),
+      body: ListView(
+        children: [
+          CarouselSlider(
+            options: CarouselOptions(
+              height: 200.0,
+              viewportFraction: 1,
+              autoPlay: true,
+              autoPlayInterval: Duration(seconds: 1),
+            ),
+            items: [1, 2, 3].map(
+              (value) {
+                return Builder(
+                  builder: (BuildContext context) {
+                    return Container(
+                      width: MediaQuery.of(context).size.width,
+                      margin: EdgeInsets.symmetric(horizontal: 5.0),
+                      decoration: BoxDecoration(
+                        image: DecorationImage(image: AssetImage('assets/slider/$value.png'), fit: BoxFit.cover),
+                      ),
+                    );
+                  },
+                );
+              },
+            ).toList(),
+          ),
+          Container(
+            height: 120,
+            padding: EdgeInsets.all(10),
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: List.generate(10, (index) => index)
+                  .map((e) => Padding(
+                        padding: const EdgeInsets.all(5.0),
+                        child: Column(
+                          children: [
+                            CircleAvatar(
+                              radius: 30,
+                              backgroundImage: AssetImage('assets/slider/${(e % 3) + 1}.png'),
+                            ),
+                            Text('Category $e')
+                          ],
+                        ),
+                      ))
+                  .toList(),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
